b56a19dc24f4b61d06d76c39e94ae4a8cee05c1a
Omar <omarmuhammed1998.om@gmail.com>
2020-12-22 12:50:39 +0200

    implemented edit_artist_submission

--- app.py
+++ app.py
@@ -282,15 +282,33 @@ def show_artist(artist_id):
 def edit_artist(artist_id):
     artist = Artist.query.get(artist_id)
     form = ArtistForm(obj=artist)
-    # TODO: populate form with fields from artist with ID <artist_id>
+    # TODO-: populate form with fields from artist with ID <artist_id>(Done!)
     return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-    # TODO: take values from the form submitted, and update existing
+    # TODO-: take values from the form submitted, and update existing(Done!)
     # artist record with ID <artist_id> using the new attributes
-
+    try:
+        form = ArtistForm(request.form)
+        artist = Artist.query.get(artist_id)
+        artist.name = form.name.data
+        artist.city = form.city.data
+        artist.state = form.state.data
+        artist.phone = form.phone.data
+        artist.genres = form.genres.data
+        artist.image_link = form.image_link.data
+        artist.facebook_link = form.facebook_link.data
+        artist.website = form.website.data
+        artist.seeking_venue = form.seeking_venue.data
+        artist.seeking_description = form.seeking_description.data
+        db.session.commit()
+    except:
+        db.session.rollback()
+        print(exc_info)
+    finally:
+        db.session.close()
     return redirect(url_for('show_artist', artist_id=artist_id))
 
 
