2c83dfcba2dccfb7de465da732be1036077a1161
Omar <omarmuhammed1998.om@gmail.com>
2020-12-22 02:08:29 +0200

    Implemented create_artist_submission

--- app.py
+++ app.py
@@ -378,14 +378,48 @@ def create_artist_form():
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
     # called upon submitting the new artist listing form
-    # TODO: insert form data as a new Venue record in the db, instead
-    # TODO: modify data to be the data object returned from db insertion
-
-    # on successful db insert, flash success
-    flash('Artist ' + request.form['name'] + ' was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
-    return render_template('pages/home.html')
+    # TODO-: insert form data as a new Venue record in the db, instead(Done!)
+    form = ArtistForm(request.form)
+    data = {
+        'name': form.name.data,
+        'genres': form.genres.data,
+        'city': form.city.data,
+        'state': form.state.data,
+        'phone': form.phone.data,
+        'image_link': form.image_link.data,
+        'facebook_link': form.facebook_link.data,
+        'seeking_talent': form.seeking_venue.data,
+        'seeking_description': form.seeking_description.data,
+        'website': form.website.data
+    }
+    # TODO-: modify data to be the data object returned from db insertion(Done!)
+    try: 
+        artist = Artist(
+            name=data['name'], 
+            genres=data['genres'], 
+            city=data['city'],
+            state=data['state'],
+            phone=data['phone'],
+            image_link=data['image_link'],
+            facebook_link=data['facebook_link'],
+            seeking_venue=data['seeking_talent'],
+            seeking_description=data['seeking_description'],
+            website=data['website']
+        )
+        db.session.add(artist)
+        db.session.commit()
+        # on successful db insert, flash success
+        flash('Artist ' + data['name'] + ' was successfully listed!')
+    except:
+        # TODO-: on unsuccessful db insert, flash an error instead.(Done!)
+        # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
+        # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+        flash('An error occurred. Artist ' + data['name'] + ' could not be listed.')
+        db.session.rollback()
+        print(exc_info())
+    finally:
+        db.session.close() 
+    return redirect(url_for('index'))
 
 
 #  Shows
